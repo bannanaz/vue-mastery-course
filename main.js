742b9b73edc86691c7a119dd29b9530525074dc1
bannanaz <anlepet@gmail.com>
2021-11-16 13:32:31 +0100

    Computed poroperties

--- main.js
+++ main.js
@@ -2,19 +2,23 @@ var app = new Vue({
   el: "#app",
   data: {
     product: "Socks",
-    image: "./assets/vmSocks-green-onWhite.jpeg",
-    inStock: true,
+    brand: "Vue Mastery",
+    selectedVariant: 0,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
     variants: [
       {
         variantId: 2234,
         variantColor: "green",
         variantImage: "./assets/vmSocks-green-onWhite.jpeg",
+        variantQuantity: 100,
+        onSale: true,
       },
       {
         variantId: 2235,
         variantColor: "blue",
         variantImage: "./assets/vmSocks-blue-onWhite.jpeg",
+        variantQuantity: 0,
+        onSale: false,
       },
     ],
     sizes: [
@@ -30,8 +34,23 @@ var app = new Vue({
     subtractFromCart() {
       this.cart -= 1;
     },
-    updateProduct(variantImage) {
-      this.image = variantImage;
+    updateProduct(index) {
+      this.selectedVariant = index;
+      console.log(index);
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity;
+    },
+    onSale() {
+      return this.variants[this.selectedVariant].onSale;
     },
   },
 });

1785aa7bc89ae769f7606f20710838d115bdfaf7
bannanaz <anlepet@gmail.com>
2021-11-16 11:05:15 +0100

    List rendering

--- main.js
+++ main.js
@@ -3,7 +3,15 @@ var app = new Vue({
   data: {
     product: "Socks",
     image: "./assets/vmSocks-green-onWhite.jpeg",
-    inventory: 100,
-    onsale: true,
+    inStock: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      { variantId: 2234, variantColor: "green" },
+      { variantId: 2235, variantColor: "blue" },
+    ],
+    sizes: [
+      { sizeVariant: "S", sizeId: 1 },
+      { sizeVariant: "M", sizeId: 2 },
+    ],
   },
 });

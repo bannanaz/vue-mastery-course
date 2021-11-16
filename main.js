a59340be035ac582043d777922f2884b4080045b
bannanaz <anlepet@gmail.com>
2021-11-16 11:30:11 +0100

    Event Handlers

--- main.js
+++ main.js
@@ -6,12 +6,32 @@ var app = new Vue({
     inStock: true,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
     variants: [
-      { variantId: 2234, variantColor: "green" },
-      { variantId: 2235, variantColor: "blue" },
+      {
+        variantId: 2234,
+        variantColor: "green",
+        variantImage: "./assets/vmSocks-green-onWhite.jpeg",
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+        variantImage: "./assets/vmSocks-blue-onWhite.jpeg",
+      },
     ],
     sizes: [
       { sizeVariant: "S", sizeId: 1 },
       { sizeVariant: "M", sizeId: 2 },
     ],
+    cart: 0,
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    subtractFromCart() {
+      this.cart -= 1;
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage;
+    },
   },
 });
